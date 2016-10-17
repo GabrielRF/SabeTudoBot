e866ccdaa4223cef0e4d747b3a18b529ead2f3b9
GabrielRF <github@gabrf.com>
2016-10-17 09:32:20 -0400

    bug fix

--- sabetudo.py
+++ sabetudo.py
@@ -63,7 +63,7 @@ def query_text(inline_query):
             types.InputTextMessageContent(str(u'\U0001F52E') + ' ' +random.choice(valor)))
         c = types.InlineQueryResultArticle('3', 'Culpados.',
             types.InputTextMessageContent(str(u'\U0001F52E') + ' ' +random.choice(culpa)))
-        bot.answer_inline_query(inline_query.id, [s,d,v], cache_time=1, is_personal=True)
+        bot.answer_inline_query(inline_query.id, [s,d,v,c], cache_time=1, is_personal=True)
     except Exception as e:
         print(e)
 
